f00789661ab5e87afbf4ee7bc6bd998b9467d43e
maihu <maihu@DESKTOP-KL3KVFG>
2019-11-16 14:23:24 +0700

    14:23

--- src/helper/JdbcHelper.java
+++ src/helper/JdbcHelper.java
@@ -18,7 +18,7 @@ import java.sql.SQLException;
 public class JdbcHelper {
 
     private static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    private static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=PolyPro";
+    private static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=DB_HiBank";
     private static String username = "sa";
     private static String password = "123";
 
