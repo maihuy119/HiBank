7500b924dc13825567fcd14659930b4bd1d1e733
maihu <maihu@DESKTOP-KL3KVFG>
2019-11-19 13:49:56 +0700

    model

--- src/dao/NhanVienDAO.java
+++ src/dao/NhanVienDAO.java
@@ -6,6 +6,10 @@
 package dao;
 
 import helper.JdbcHelper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import model.NhanVien;
 
 /**
@@ -13,11 +17,12 @@ import model.NhanVien;
  * @author maihu
  */
 public class NhanVienDAO {
+
     public void insert(NhanVien model) {
-        String sql = "INSERT INTO tbl_NhanVien (MaNV, MatKhau, HoTen, VaiTro) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO tbl_NhanVien (ma_nv, ho_ten, gioi_tinh, ngay_sinh, cmnd, que_quan, dia_chi_thuong_tru, so_dien_thoai, chuc_vu, luong, ngay_cong_tac, ghi_chu, anh) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         JdbcHelper.executeUpdate(sql,
                 model.getMaNV(),
-                model.getMatKhau(),
+                model.getHoTen(),
                 model.getHoTen(),
                 model.isVaiTro());
     }
@@ -32,7 +37,47 @@ public class NhanVienDAO {
     }
 
     public void delete(String MaNV) {
-        String sql = "DELETE FROM NhanVien WHERE MaNV=?";
+        String sql = "DELETE FROM tbl_NhanVien WHERE MaNV=?";
         JdbcHelper.executeUpdate(sql, MaNV);
     }
+
+    public List<NhanVien> select() {
+        String sql = "SELECT * FROM tbl_NhanVien";
+        return select(sql);
+    }
+
+    public NhanVien findById(String manv) {
+        String sql = "SELECT * FROM tbl_NhanVien WHERE MaNV=?";
+        List<NhanVien> list = select(sql, manv);
+        return list.size() > 0 ? list.get(0) : null;
+    }
+
+    private List<NhanVien> select(String sql, Object... args) {
+        List<NhanVien> list = new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                rs = JdbcHelper.executeQuery(sql, args);
+                while (rs.next()) {
+                    NhanVien model = readFromResultSet(rs);
+                    list.add(model);
+                }
+            } finally {
+                rs.getStatement().getConnection().close();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        return list;
+    }
+
+    private NhanVien readFromResultSet(ResultSet rs) throws SQLException {
+        NhanVien model = new NhanVien();
+        model.setMaNV(rs.getString("MaNV"));
+        model.setMatKhau(rs.getString("MatKhau"));
+        model.setHoTen(rs.getString("HoTen"));
+        model.setVaiTro(rs.getBoolean("VaiTro"));
+        return model;
+    }
+
 }
