133e078ba27286291066e23d7aa99dc7b8fbe716
maihu <maihu@DESKTOP-KL3KVFG>
2019-11-19 13:40:02 +0700

    model

--- src/dao/NhanVienDAO.java
+++ src/dao/NhanVienDAO.java
@@ -5,10 +5,34 @@
  */
 package dao;
 
+import helper.JdbcHelper;
+import model.NhanVien;
+
 /**
  *
  * @author maihu
  */
 public class NhanVienDAO {
-    
+    public void insert(NhanVien model) {
+        String sql = "INSERT INTO tbl_NhanVien (MaNV, MatKhau, HoTen, VaiTro) VALUES (?, ?, ?, ?)";
+        JdbcHelper.executeUpdate(sql,
+                model.getMaNV(),
+                model.getMatKhau(),
+                model.getHoTen(),
+                model.isVaiTro());
+    }
+
+    public void update(NhanVien model) {
+        String sql = "UPDATE NhanVien SET MatKhau=?, HoTen=?, VaiTro=? WHERE MaNV=?";
+        JdbcHelper.executeUpdate(sql,
+                model.getMatKhau(),
+                model.getHoTen(),
+                model.isVaiTro(),
+                model.getMaNV());
+    }
+
+    public void delete(String MaNV) {
+        String sql = "DELETE FROM NhanVien WHERE MaNV=?";
+        JdbcHelper.executeUpdate(sql, MaNV);
+    }
 }
